66915ffb31e3b6559e2d4f7666e97925a43d9488
paccy-dev <paccydev@gmail.com>
2025-01-13 15:27:27 +0200

    database mig

--- database/migrations/2024_12_30_004224_create_patients_table.php
+++ database/migrations/2024_12_30_004224_create_patients_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::update('patients', function (Blueprint $table) {
+        Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->string('full_name');
             $table->string('birth_place');
